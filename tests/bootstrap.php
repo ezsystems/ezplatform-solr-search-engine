52b467b2b27e9de3c47e182f05d410e324c9c30e
Petar Španja <petar.spanja@gmail.com>
2015-07-23 17:43:31 +0200

    EZP-24619: setup config.php for integration tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,6 +9,14 @@
  * @version //autogentag//
  */
 
+// Setup config file for running integration tests from ezpublish-kernel dependency
+$file = __DIR__ . '/../vendor/ezsystems/ezpublish-kernel/config.php';
+if (!file_exists($file)) {
+    if (!symlink($file . '-DEVELOPMENT', $file)) {
+        throw new RuntimeException('Could not symlink config.php-DEVELOPMENT to config.php');
+    }
+}
+
 $file = __DIR__ . '/../vendor/autoload.php';
 if (!file_exists($file)) {
     throw new RuntimeException('Install dependencies using composer to run the test suite.');
