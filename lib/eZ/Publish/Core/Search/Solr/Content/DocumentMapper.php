2381f9a6c0e4cc3167370fb3447f0612dfc5937b
Petar Španja <petar.spanja@gmail.com>
2015-07-29 11:53:15 +0200

    EZP-24649: update document mapper for document blocks

--- lib/eZ/Publish/Core/Search/Solr/Content/DocumentMapper.php
+++ lib/eZ/Publish/Core/Search/Solr/Content/DocumentMapper.php
@@ -15,7 +15,7 @@ use eZ\Publish\SPI\Persistence\Content;
 use eZ\Publish\SPI\Persistence\Content\Location;
 
 /**
- * Mapper maps Content and Location objects to a Document object, representing a
+ * Mapper maps Content and Location objects to a Document objects, representing a
  * document in Solr index storage.
  *
  * Note that custom implementations might need to be accompanied by custom schema.
@@ -23,20 +23,44 @@ use eZ\Publish\SPI\Persistence\Content\Location;
 interface DocumentMapper
 {
     /**
-     * Maps given Content to a Document.
+     * Maps given Content and it's Locations to a collection of nested Documents,
+     * one per translation.
+     *
+     * Each Content Document contains nested Documents representing it's Locations.
      *
      * @param \eZ\Publish\SPI\Persistence\Content $content
      *
      * @return \eZ\Publish\SPI\Search\Document[]
      */
-    public function mapContent(Content $content);
+    public function mapContentBlock(Content $content);
 
     /**
-     * Maps given Location to a Document.
+     * Generates the Solr backend document ID for Content object.
      *
-     * @param \eZ\Publish\SPI\Persistence\Content\Location $location
+     * If $language code is not provided, the method will return prefix of the IDs
+     * of all Content's documents (there will be one document per translation).
+     * The above is useful when targeting all Content's documents, without
+     * the knowledge of it's translations.
      *
-     * @return \eZ\Publish\SPI\Search\Document[]
+     * @param int|string $contentId
+     * @param string $languageCode
+     *
+     * @return string
      */
-    public function mapLocation(Location $location);
+    public function generateContentDocumentId($contentId, $languageCode = null);
+
+    /**
+     * Generates the Solr backend document ID for Location object.
+     *
+     * If $language code is not provided, the method will return prefix of the IDs
+     * of all Location's documents (there will be one document per translation).
+     * The above is useful when targeting all Location's documents, without
+     * the knowledge of it's Content's translations.
+     *
+     * @param int|string $locationId
+     * @param string $languageCode
+     *
+     * @return string
+     */
+    public function generateLocationDocumentId($locationId, $languageCode = null);
 }
