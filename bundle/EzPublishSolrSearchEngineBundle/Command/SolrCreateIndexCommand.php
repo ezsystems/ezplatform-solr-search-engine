c57285d642864f55114629c4fa5536099cab4cab
Petar Španja <petar.spanja@gmail.com>
2015-08-03 20:49:08 +0200

    EZP-24664: updated indexing command

--- bundle/EzPublishSolrSearchEngineBundle/Command/SolrCreateIndexCommand.php
+++ bundle/EzPublishSolrSearchEngineBundle/Command/SolrCreateIndexCommand.php
@@ -60,15 +60,9 @@ EOT
         $stmt = $query->prepare();
         $stmt->execute();
 
-        /** @var \eZ\Publish\Core\Search\Solr\Content\Handler $contentSearchHandler */
-        $contentSearchHandler = $searchHandler->contentSearchHandler();
-        $contentSearchHandler->setCommit(true);
-        $contentSearchHandler->purgeIndex();
-
-        /** @var \eZ\Publish\Core\Search\Solr\Content\Location\Handler $locationSearchHandler */
-        $locationSearchHandler = $searchHandler->locationSearchHandler();
-        $locationSearchHandler->setCommit(true);
-        $locationSearchHandler->purgeIndex();
+        /** @var \eZ\Publish\Core\Search\Solr\Handler $searchHandler */
+        $searchHandler->setCommit(true);
+        $searchHandler->purgeIndex();
 
         $output->writeln('Indexing Content...');
 
@@ -91,62 +85,7 @@ EOT
             }
 
             if (!empty($contentObjects)) {
-                $contentSearchHandler->bulkIndexContent($contentObjects);
-            }
-
-            $progress->advance($k);
-        } while (($i += $bulkCount) < $totalCount);
-
-        $progress->finish();
-
-        // Indexing Locations
-        $query = $databaseHandler->createSelectQuery();
-        $query
-            ->select('count(node_id)')
-            ->from('ezcontentobject_tree')
-            ->where(
-                $query->expr->neq(
-                    $databaseHandler->quoteColumn('contentobject_id'),
-                    $query->bindValue(0, null, PDO::PARAM_INT)
-                )
-            );
-        $stmt = $query->prepare();
-        $stmt->execute();
-        $totalCount = $stmt->fetchColumn();
-
-        $query = $databaseHandler->createSelectQuery();
-        $query
-            ->select('node_id')
-            ->from('ezcontentobject_tree')
-            ->where(
-                $query->expr->neq(
-                    $databaseHandler->quoteColumn('contentobject_id'),
-                    $query->bindValue(0, null, PDO::PARAM_INT)
-                )
-            );
-
-        $stmt = $query->prepare();
-        $stmt->execute();
-
-        $output->writeln('Indexing Locations...');
-
-        /** @var \Symfony\Component\Console\Helper\ProgressHelper $progress */
-        $progress = $this->getHelperSet()->get('progress');
-        $progress->start($output, $totalCount);
-        $i = 0;
-        do {
-            $locations = array();
-
-            for ($k = 0; $k <= $bulkCount; ++$k) {
-                if (!$row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                    break;
-                }
-
-                $locations[] = $persistenceHandler->locationHandler()->load($row['node_id']);
-            }
-
-            if (!empty($locations)) {
-                $locationSearchHandler->bulkIndexLocations($locations);
+                $searchHandler->bulkIndexContent($contentObjects);
             }
 
             $progress->advance($k);
