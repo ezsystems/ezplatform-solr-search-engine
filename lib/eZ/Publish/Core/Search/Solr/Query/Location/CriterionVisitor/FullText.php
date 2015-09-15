8798a6d8b8322170833bcea3f51b7925b4bb7474
Petar Španja <petar.spanja@gmail.com>
2015-09-15 12:07:58 +0200

    EZP-24802: implement full text Location search using child block join

--- lib/eZ/Publish/Core/Search/Solr/Query/Location/CriterionVisitor/FullText.php
+++ lib/eZ/Publish/Core/Search/Solr/Query/Location/CriterionVisitor/FullText.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * File containing the FullText criterion visitor class for Location Search.
+ * This file is part of the eZ Publish Kernel package.
  *
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
@@ -11,10 +11,26 @@
 namespace eZ\Publish\Core\Search\Solr\Query\Location\CriterionVisitor;
 
 use eZ\Publish\Core\Search\Solr\Query\Content\CriterionVisitor\FullText as ContentFullText;
+use eZ\Publish\Core\Search\Solr\Query\CriterionVisitor;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
 
 /**
  * Visits the FullText criterion.
  */
 class FullText extends ContentFullText
 {
+    /**
+     * Map field value to a proper Solr representation.
+     *
+     * @param \eZ\Publish\API\Repository\Values\Content\Query\Criterion $criterion
+     * @param \eZ\Publish\Core\Search\Solr\Query\CriterionVisitor $subVisitor
+     *
+     * @return string
+     */
+    public function visit(Criterion $criterion, CriterionVisitor $subVisitor = null)
+    {
+        $condition = $this->escapeQuote(parent::visit($criterion, $subVisitor));
+
+        return "{!child of='document_type_id:content' v='document_type_id:content AND {$condition}'}";
+    }
 }
