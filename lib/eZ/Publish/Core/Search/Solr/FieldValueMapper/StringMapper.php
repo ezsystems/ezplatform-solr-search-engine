8daef0dc023b364146ee45857d7e8a85bfa6a58c
Petar Španja <petar.spanja@gmail.com>
2015-09-21 19:54:50 +0200

    EZP-23247: updated: value is now scalar only

--- lib/eZ/Publish/Core/Search/Solr/FieldValueMapper/StringMapper.php
+++ lib/eZ/Publish/Core/Search/Solr/FieldValueMapper/StringMapper.php
@@ -13,7 +13,6 @@ namespace eZ\Publish\Core\Search\Solr\FieldValueMapper;
 use eZ\Publish\Core\Search\Solr\FieldValueMapper;
 use eZ\Publish\SPI\Search\Field;
 use eZ\Publish\SPI\Search\FieldType;
-use DOMDocument;
 
 /**
  * Maps raw document field values to something Solr can index.
@@ -56,6 +55,6 @@ class StringMapper extends FieldValueMapper
     protected function convert($value)
     {
         // Remove non-printables
-        return preg_replace('([\x00-\x09\x0B\x0C\x1E\x1F]+)', '', $value instanceof DOMDocument ? $value->saveXML() : $value);
+        return preg_replace('([\x00-\x09\x0B\x0C\x1E\x1F]+)', '', (string)$value);
     }
 }
