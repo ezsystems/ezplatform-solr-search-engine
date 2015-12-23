a200c36cb0d8ed246d522c15e3cc749ed4484f87
Tyler Harms <tyler.harms@gmail.com>
2015-12-23 13:43:12 -0600

    update concat to respect psr2 standards

--- lib/Query/Location/CriterionVisitor/PermissionSubtree.php
+++ lib/Query/Location/CriterionVisitor/PermissionSubtree.php
@@ -33,6 +33,6 @@ class PermissionSubtree extends CriterionVisitor
     public function visit(Criterion $criterion, CriterionVisitor $subVisitor = null)
     {
         $subtrees = $criterion->value;
-        return 'path_string_id:['.implode('* ', $subtrees).'*]';
+        return 'path_string_id:[' . implode('* ', $subtrees) . '*]';
     }
 }
