ddcf350d4cf2a12f1ed20a50370f27a1e890f4e5
Petar Španja <petar@spanja.info>
2016-12-16 15:02:16 +0100

    Add deprecated implementation for BC reasons (#85)
    * Add deprecated implementation for BC reasons
    
    * Mark abstract Field criterion visitor as api

---
+++ lib/Query/Content/CriterionVisitor/Field.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of the eZ Platform Solr Search Engine package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\EzPlatformSolrSearchEngine\Query\Content\CriterionVisitor;
+
+use EzSystems\EzPlatformSolrSearchEngine\Query\Common\CriterionVisitor\Field as FieldBase;
+
+/**
+ * Kept for BC reasons.
+ *
+ * @deprecated since 1.2, to be removed in 2.0. Use extended class instead.
+ *
+ * @see \EzSystems\EzPlatformSolrSearchEngine\Query\Common\CriterionVisitor\Field
+ */
+abstract class Field extends FieldBase
+{
+}
