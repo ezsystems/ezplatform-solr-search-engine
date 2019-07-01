5dd307f25c36460c1ad4c4ad7d7d7fb7788d7eba
Douglas Hammond <wizhippo@gmail.com>
2019-07-01 04:25:07 -0400

    EZP-28898: Implemented SortClause visitor for Random sort (#118)
    * Implemented SortClause visitor for Random sort
    
    * Added random seed if non provided

---
+++ lib/Query/Common/SortClauseVisitor/Random.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * This file is part of the eZ Platform Solr Search Engine package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+namespace EzSystems\EzPlatformSolrSearchEngine\Query\Common\SortClauseVisitor;
+
+use EzSystems\EzPlatformSolrSearchEngine\Query\SortClauseVisitor;
+use eZ\Publish\API\Repository\Values\Content\Query\SortClause;
+
+/**
+ * Visits the sortClause tree into a Solr query.
+ */
+class Random extends SortClauseVisitor
+{
+    /**
+     * Check if visitor is applicable to current sortClause.
+     *
+     * @param SortClause $sortClause
+     *
+     * @return bool
+     */
+    public function canVisit(SortClause $sortClause)
+    {
+        return $sortClause instanceof SortClause\Random;
+    }
+
+    /**
+     * Map field value to a proper Solr representation.
+     *
+     * @param SortClause $sortClause
+     *
+     * @return string
+     */
+    public function visit(SortClause $sortClause)
+    {
+        $seed = $sortClause->targetData->seed ?? mt_rand();
+        return 'random_' . (string)$seed . ' ' . $this->getDirection($sortClause);
+    }
+}
