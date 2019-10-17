f7c78d22df6130f4f376676057760e7bff0a276d
Mateusz Bieniek <mateusz.bieniek@ez.no>
2019-10-17 09:12:19 +0200

    Removed changes for Null Searching

--- lib/Query/Common/CriterionVisitor/Field/FieldIn.php
+++ lib/Query/Common/CriterionVisitor/Field/FieldIn.php
@@ -8,39 +8,17 @@
  */
 namespace EzSystems\EzPlatformSolrSearchEngine\Query\Common\CriterionVisitor\Field;
 
-use eZ\Publish\SPI\Search\FieldType\BooleanField;
-use EzSystems\EzPlatformSolrSearchEngine\FieldMapper\ContentTranslationFieldMapper\ContentDocumentNullFields;
 use EzSystems\EzPlatformSolrSearchEngine\Query\CriterionVisitor;
 use EzSystems\EzPlatformSolrSearchEngine\Query\Common\CriterionVisitor\Field;
 use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
 use eZ\Publish\API\Repository\Values\Content\Query\Criterion\Operator;
 use eZ\Publish\Core\Base\Exceptions\InvalidArgumentException;
-use eZ\Publish\Core\Search\Common\FieldValueMapper;
-use eZ\Publish\Core\Search\Common\FieldNameResolver;
-use eZ\Publish\Core\Search\Common\FieldNameGenerator;
 
 /**
  * Visits the Field criterion.
  */
 class FieldIn extends Field
 {
-    /**
-     * @var \eZ\Publish\Core\Search\Common\FieldNameGenerator
-     */
-    private $fieldNameGenerator;
-
-    /**
-     * @param \eZ\Publish\Core\Search\Common\FieldNameResolver $fieldNameResolver
-     * @param \eZ\Publish\Core\Search\Common\FieldValueMapper $fieldValueMapper
-     * @param \eZ\Publish\Core\Search\Common\FieldNameGenerator $fieldNameGenerator
-     */
-    public function __construct(FieldNameResolver $fieldNameResolver, FieldValueMapper $fieldValueMapper, FieldNameGenerator $fieldNameGenerator)
-    {
-        parent::__construct($fieldNameResolver, $fieldValueMapper);
-
-        $this->fieldNameGenerator = $fieldNameGenerator;
-    }
-
     /**
      * Check if visitor is applicable to current criterion.
      *
@@ -78,33 +56,19 @@ class FieldIn extends Field
             );
         }
 
-        if ($criterion->value === null) {
-            $criterion->value[] = null;
-        } else {
-            $criterion->value = (array)$criterion->value;
-        }
+        $criterion->value = (array)$criterion->value;
         $queries = array();
 
         foreach ($searchFields as $name => $fieldType) {
             foreach ($criterion->value as $value) {
-                if ($value === null) {
-                    $name = $this->fieldNameGenerator->getTypedName(
-                        $this->fieldNameGenerator->getName(
-                            ContentDocumentNullFields::IS_NULL_NAME,
-                            $criterion->target
-                        ),
-                        new BooleanField()
-                    );
-                    $queries[] = $name . ':true';
-                } else {
-                    $preparedValue = $this->escapeQuote(
-                        $this->toString(
-                            $this->mapSearchFieldValue($value, $fieldType)
-                        ),
-                        true
-                    );
-                    $queries[] = $name . ':"' . $preparedValue . '"';
-                }
+                $preparedValue = $this->escapeQuote(
+                    $this->toString(
+                        $this->mapSearchFieldValue($value, $fieldType)
+                    ),
+                    true
+                );
+
+                $queries[] = $name . ':"' . $preparedValue . '"';
             }
         }
 
