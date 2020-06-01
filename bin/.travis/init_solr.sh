c84417b56444262f7d2c10f08ff6a888bdf46776
Adam Wójs <adam@wojs.pl>
2020-06-01 15:17:08 +0200

    Run tests on Solr 8

--- bin/.travis/init_solr.sh
+++ bin/.travis/init_solr.sh
@@ -45,7 +45,7 @@ fi
 download() {
     case ${SOLR_VERSION} in
         # PS!!: Append versions and don't remove old once, kernel uses this script!
-        7.7.2 )
+        7.7.3|8.5.2 )
             url="http://archive.apache.org/dist/lucene/solr/${SOLR_VERSION}/solr-${SOLR_VERSION}.tgz"
             ;;
         *)
