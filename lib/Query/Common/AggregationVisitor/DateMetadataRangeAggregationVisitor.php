e571fd037830f8ac518ec5702dc30e0bc0c46e73
Adam Wójs <adam@wojs.pl>
2020-08-28 12:42:18 +0200

    Added DateMetadataRangeAggregationVisitor and DateRangeAggregationResultExtractor

---
+++ lib/Query/Common/AggregationVisitor/DateMetadataRangeAggregationVisitor.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformSolrSearchEngine\Query\Common\AggregationVisitor;
+
+use eZ\Publish\API\Repository\Values\Content\Query\Aggregation\AbstractRangeAggregation;
+use eZ\Publish\API\Repository\Values\Content\Query\Aggregation\DateMetadataRangeAggregation;
+use eZ\Publish\API\Repository\Values\Content\Query\AggregationInterface;
+use RuntimeException;
+
+final class DateMetadataRangeAggregationVisitor extends AbstractRangeAggregationVisitor
+{
+    public function canVisit(AggregationInterface $aggregation, array $languageFilter): bool
+    {
+        return $aggregation instanceof DateMetadataRangeAggregation;
+    }
+
+    /**
+     * @param \eZ\Publish\API\Repository\Values\Content\Query\Aggregation\DateMetadataRangeAggregation $aggregation
+     */
+    protected function getTargetField(AbstractRangeAggregation $aggregation): string
+    {
+        switch ($aggregation->getType()) {
+            case DateMetadataRangeAggregation::PUBLISHED:
+                return 'content_publication_date_dt';
+            case DateMetadataRangeAggregation::MODIFIED:
+                return 'content_modification_date_dt';
+            default:
+                throw new RuntimeException("Unsupported DateMetadataRangeAggregation type {$aggregation->getType()}");
+        }
+    }
+}
---
+++ lib/ResultExtractor/AggregationResultExtractor/DateRangeAggregationResultExtractor.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformSolrSearchEngine\ResultExtractor\AggregationResultExtractor;
+
+use DateTimeImmutable;
+use DateTimeInterface;
+use eZ\Publish\API\Repository\Values\Content\Query\Aggregation\Range;
+use eZ\Publish\API\Repository\Values\Content\Query\AggregationInterface;
+use eZ\Publish\API\Repository\Values\Content\Search\AggregationResult;
+use eZ\Publish\API\Repository\Values\Content\Search\AggregationResult\RangeAggregationResult;
+use eZ\Publish\API\Repository\Values\Content\Search\AggregationResult\RangeAggregationResultEntry;
+use EzSystems\EzPlatformSolrSearchEngine\ResultExtractor\AggregationResultExtractor;
+use stdClass;
+
+final class DateRangeAggregationResultExtractor implements AggregationResultExtractor
+{
+    /** @var string */
+    private $aggregationClass;
+
+    public function __construct(string $aggregationClass)
+    {
+        $this->aggregationClass = $aggregationClass;
+    }
+
+    public function canVisit(AggregationInterface $aggregation, array $languageFilter): bool
+    {
+        return $aggregation instanceof $this->aggregationClass;
+    }
+
+    public function extract(AggregationInterface $aggregation, array $languageFilter, stdClass $data): AggregationResult
+    {
+        $entries = [];
+
+        foreach ($data as $key => $bucket) {
+            if ($key === 'count') {
+                continue;
+            }
+
+            list($from, $to) = explode('_', $key);
+
+            $entries[] = new RangeAggregationResultEntry(
+                new Range(
+                    $this->getRangeBorderFromString($from),
+                    $this->getRangeBorderFromString($to),
+                ),
+                $bucket->count
+            );
+        }
+
+        return new RangeAggregationResult($aggregation->getName(), $entries);
+    }
+
+    private function getRangeBorderFromString(string $value): ?DateTimeInterface
+    {
+        if ($value === '*') {
+            return null;
+        }
+
+        return new DateTimeImmutable($value);
+    }
+}
