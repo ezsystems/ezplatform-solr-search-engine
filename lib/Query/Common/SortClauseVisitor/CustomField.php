31cdef7a1af18bb962ab9c638e9ba5f7993aace1
Adam Wójs <adam.wojs@ez.no>
2020-12-10 13:22:05 +0100

    EZP-32108: Added CustomField sort clause (#204)

---
+++ lib/Query/Common/SortClauseVisitor/CustomField.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformSolrSearchEngine\Query\Common\SortClauseVisitor;
+
+use eZ\Publish\API\Repository\Values\Content\Query\SortClause;
+use EzSystems\EzPlatformSolrSearchEngine\Query\SortClauseVisitor;
+
+final class CustomField extends SortClauseVisitor
+{
+    public function canVisit(SortClause $sortClause): bool
+    {
+        return $sortClause instanceof SortClause\CustomField;
+    }
+
+    public function visit(SortClause $sortClause): string
+    {
+        /** @var \eZ\Publish\API\Repository\Values\Content\Query\SortClause\Target\CustomFieldTarget $targetData */
+        $targetData = $sortClause->targetData;
+
+        return $targetData->fieldName . ' ' . $this->getDirection($sortClause);
+    }
+}
