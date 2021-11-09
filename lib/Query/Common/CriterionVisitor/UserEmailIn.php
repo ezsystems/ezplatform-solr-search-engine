8a24559d65b52d23c8fa3afa4fcd1fc94400c787
Paweł Niedzielski <pawel.tadeusz.niedzielski@gmail.com>
2021-11-09 10:28:06 +0100

    Added UserEmailIn Criterion visitor

---
+++ lib/Query/Common/CriterionVisitor/UserEmailIn.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace EzSystems\EzPlatformSolrSearchEngine\Query\Common\CriterionVisitor;
+
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion\Operator;
+use EzSystems\EzPlatformSolrSearchEngine\Query\CriterionVisitor;
+
+final class UserEmailIn extends CriterionVisitor
+{
+    public function canVisit(Criterion $criterion): bool
+    {
+        $operator = $criterion->operator ?? Operator::IN;
+        $supportedOperators = [Operator::IN, Operator::EQ];
+
+        return
+            $criterion instanceof Criterion\UserEmail
+            && in_array($operator, $supportedOperators, true);
+    }
+
+    public function visit(Criterion $criterion, CriterionVisitor $subVisitor = null): string
+    {
+        return sprintf(
+            '(%s)',
+            implode(
+                ' OR ',
+                array_map(
+                    static function (string $value): string {
+                        return 'user_user_account_user_email_s:"' . $value . '"';
+                    },
+                    (array) $criterion->value
+                )
+            )
+        );
+    }
+}
