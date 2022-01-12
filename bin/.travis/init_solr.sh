c1da8716c7c99a65d08a8652aaac04fe4b40d7ef
Gunnstein Lye <gunnstein.lye@ez.no>
2022-01-12 13:53:11 +0100

    IBX-1699: Bump requirement for Solr (#228)

--- bin/.travis/init_solr.sh
+++ bin/.travis/init_solr.sh
@@ -16,7 +16,7 @@ default_shards=('shard0')
 
 SOLR_PORT=${SOLR_PORT:-8983}
 SOLR_DIR=${SOLR_DIR:-'__solr'}
-SOLR_VERSION=${SOLR_VERSION:-'7.7.2'}
+SOLR_VERSION=${SOLR_VERSION:-'8.11.1'}
 SOLR_INSTALL_DIR="${SOLR_DIR}/${SOLR_VERSION}"
 SOLR_DEBUG=${SOLR_DEBUG:-false}
 SOLR_HOME=${SOLR_HOME:-'ezcloud'}
@@ -44,8 +44,8 @@ fi
 
 download() {
     case ${SOLR_VERSION} in
-        # PS!!: Append versions and don't remove old once, kernel uses this script!
-        7.7.* | 8.[5-8].* )
+        # PS!!: Append versions and don't remove old ones, kernel uses this script!
+        7.7.* | 8.[5-8].* | 8.11.* )
             url="http://archive.apache.org/dist/lucene/solr/${SOLR_VERSION}/solr-${SOLR_VERSION}.tgz"
             ;;
         *)
@@ -132,7 +132,7 @@ wait_for_solr(){
     done
 }
 
-# Run for Solr 7
+# Run for Solr 7+
 solr_run() {
     echo "Running with version ${SOLR_VERSION} in standalone mode"
     echo "Starting solr on port ${SOLR_PORT}..."
@@ -144,7 +144,7 @@ solr_run() {
     solr_create_cores
 }
 
-# Create cores for Solr 7
+# Create cores for Solr 7+
 solr_create_cores() {
     home_dir="${SOLR_INSTALL_DIR}/server/${SOLR_HOME}"
     template_dir="${home_dir}/template"
